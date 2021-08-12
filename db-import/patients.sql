082b4aca3617e8ef90017213c13e84716bbc2676
zahraAfa <anisafatimahazzahra@gmail.com>
2021-08-12 17:22:00 +0700

    new sql file added

--- db-import/patients.sql
+++ db-import/patients.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 09, 2021 at 10:37 AM
+-- Host: localhost
+-- Generation Time: Aug 12, 2021 at 12:20 PM
 -- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `covid-19`
+-- Database: `testcovid19`
 --
 
 -- --------------------------------------------------------
@@ -30,12 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `patients` (
   `id` int(11) NOT NULL,
   `patients_name` varchar(255) NOT NULL,
-  `patients_age` int(255) NOT NULL,
+  `patient_age` int(255) NOT NULL,
   `patients_sex` varchar(255) NOT NULL,
   `hospital_a_t` date NOT NULL,
   `icu` date DEFAULT NULL,
   `discharge` date DEFAULT NULL,
-  `death` date DEFAULT NULL,
+  `death` int(11) DEFAULT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -43,13 +43,9 @@ CREATE TABLE `patients` (
 -- Dumping data for table `patients`
 --
 
-INSERT INTO `patients` (`id`, `patients_name`, `patients_age`, `patients_sex`, `hospital_a_t`, `icu`, `discharge`, `death`, `status`) VALUES
-(1, 'Anisa Fatimah Azzahra', 21, 'Female', '2021-07-09', '0000-00-00', '2021-07-09', '0000-00-00', 'Discharge'),
-(2, 'Anisa FA', 21, 'Female', '2021-07-09', NULL, NULL, NULL, 'Admission'),
-(3, 'Anisa FA', 21, 'Female', '2021-07-09', NULL, NULL, '0000-00-00', 'Discharge'),
-(4, 'Anisa FA', 21, 'Female', '2021-07-09', NULL, NULL, NULL, 'Admission'),
-(5, 'Kang Chu Ning ', 23, 'Female', '2021-07-09', NULL, NULL, '0000-00-00', 'Discharge'),
-(6, 'Kevin Tan', 18, 'Male', '2021-07-09', NULL, NULL, NULL, 'Admission');
+INSERT INTO `patients` (`id`, `patients_name`, `patient_age`, `patients_sex`, `hospital_a_t`, `icu`, `discharge`, `death`, `status`) VALUES
+(1, 'Anisa Fatimah Azzahra', 21, 'Female', '2021-08-01', NULL, '2021-08-12', NULL, 'Discharge'),
+(2, 'Anisa Zahra', 21, 'Female', '2021-07-01', NULL, NULL, NULL, 'Admission');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +65,7 @@ ALTER TABLE `patients`
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
